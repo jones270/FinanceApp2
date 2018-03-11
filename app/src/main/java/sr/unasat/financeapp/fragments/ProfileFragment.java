0ff931b10698543340dd01dea03ec45f8b4d34bd
Jonathan Tobi <jonathantobi28@gmail.com>
2018-03-11 20:07:54 -0300

    merged 2 code bases for the second time

---
+++ app/src/main/java/sr/unasat/financeapp/fragments/ProfileFragment.java
@@ -0,0 +1,31 @@
+package sr.unasat.financeapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import sr.unasat.financeapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.profile_tab);
+        return textView;
+    }
+
+}
