2b6aceff973c72c9a35f15232a0abd7a67b45c82
Jonathan Tobi <jonathantobi28@gmail.com>
2018-03-11 15:50:01 -0300

    merged 2 code bases

--- app/src/main/java/sr/unasat/financeapp/activities/DashboardActivity.java
+++ app/src/main/java/sr/unasat/financeapp/activities/DashboardActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import sr.unasat.financeapp.R;
@@ -26,20 +27,20 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         //Attach the SectionsPagerAdapter to the ViewPager
-        SectionsPagerAdapter pagerAdapter =
-                new SectionsPagerAdapter(getSupportFragmentManager());
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
 
         //Attach the ViewPager to the TabLayout
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(pager);
     }
 
+    // Option menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -49,53 +50,24 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_create_transaction:
-                Intent intent = new Intent(this, TransactionActivity.class);
-                startActivity(intent);
-                return true;
-
-
-            case R.id.Profile:
-               // Toast.makeText(getApplicationContext(), "Welkom to my profile", Toast.LENGTH_LONG).show();
-                intent = new Intent(this, User.class);
-                startActivity(intent);
-                return true;
-
             case R.id.Logout:
-                Toast.makeText(getApplicationContext(), "You logout succsfully", Toast.LENGTH_SHORT).show();
+                logout();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-
         }
-
-
     }
 
-    public boolean onCreateOptionMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main,menu);
-        return super.onCreateOptionsMenu(menu);
-
-    }
-
-
-    public boolean onoptionsItemselected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.Logout) {
-            Logout();
-            return true;
-
-        }
-        return  super.onOptionsItemSelected(item);
-
-    }
-
-    private void Logout() {
+    private void logout() {
         Intent intent = new Intent(DashboardActivity.this,LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
+    }
+
+    public void onClickAdd(View view) {
+        Intent intentTransaction = new Intent(this, TransactionActivity.class);
+        startActivity(intentTransaction);
     }
 
 
@@ -105,7 +77,7 @@ public class DashboardActivity extends AppCompatActivity {
         }
         @Override
         public int getCount() {
-            return 2;
+            return 3;
         }
         @Override
         public Fragment getItem(int position) {
@@ -114,6 +86,8 @@ public class DashboardActivity extends AppCompatActivity {
                     return new DashboardFragment();
                 case 1:
                     return new TransactionFragment();
+                case 2:
+                    return new ProfileFragment();
             }
             return null;
         }
@@ -125,6 +99,8 @@ public class DashboardActivity extends AppCompatActivity {
                     return getResources().getText(R.string.dashboard_tab);
                 case 1:
                     return getResources().getText(R.string.transaction_tab);
+                case 2:
+                    return getResources().getText(R.string.profile_tab);
             }
             return null;
         }
---
+++ app/src/main/java/sr/unasat/financeapp/activities/ProfileActivity.java
@@ -0,0 +1,15 @@
+package sr.unasat.financeapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sr.unasat.financeapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
