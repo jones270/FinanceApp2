9d05d7ef2b5d88589adc4fec169961b753bc8a0d
jones270 <mitchelljones270@gmail.com>
2018-03-12 09:02:09 -0300

    Income & expense fragments toegevoegd dashboard renamed naar activity-main

---
+++ app/src/main/java/sr/unasat/financeapp/fragments/ExpenseTransactionFragment.java
@@ -0,0 +1,35 @@
+package sr.unasat.financeapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import sr.unasat.financeapp.R;
+import sr.unasat.financeapp.entities.Transaction;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ExpenseTransactionFragment extends Fragment {
+
+
+    public ExpenseTransactionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_expense_transaction, container, false);
+    }
+
+
+}
