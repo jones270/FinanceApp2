241463e2ead089c2bcf0d6a95371dc89e8dad3d3
Jonathan Tobi <jonathantobi28@gmail.com>
2018-03-12 09:34:25 -0300

    strings weer gezet

--- app/src/main/java/sr/unasat/financeapp/dao/TransactionDao.java
+++ app/src/main/java/sr/unasat/financeapp/dao/TransactionDao.java
@@ -39,46 +39,53 @@ public class TransactionDao {
         return newRowId;
     }
 
-//    public ArrayList<Transaction> getRecentExpenses(int dateInMiliSeconds){
-//        if(cmDbHelper == null){
-//            cmDbHelper = new ComfiDbHelper(context);
-//        }
-//
-//        SQLiteDatabase db = cmDbHelper.getReadableDatabase();
-//
-//        // Define a projection that specifies which columns from the database
-//        // you will actually use after this query.
-//        String[] projection = {
-//                ComfiContract.TransactionEntry.COLUMN_NAME_ID,
-//                ComfiContract.TransactionEntry.COLUMN_NAME_TITLE,
-//                ComfiContract.TransactionEntry.COLUMN_NAME_AMOUNT,
-//                ComfiContract.TransactionEntry.COLUMN_NAME_DATE,
-//        };
-//
-//        // Filter results WHERE "title" = 'My Title'
-//        String selection = ComfiContract.TransactionEntry.COLUMN_NAME_TYPE + " = ?" + " AND " + ComfiContract.TransactionEntry.COLUMN_NAME_DATE + " <= " + dateInMiliSeconds;
-//        String[] selectionArgs = {ComfiContract.TransactionEntry.DEFAULT_EXPENSE_STRING};
-//
-//        // How you want the results sorted in the resulting Cursor
-//        String sortOrder =
-//                ComfiContract.TransactionEntry.COLUMN_NAME_DATE + " ASC";
-//
-//        Cursor cursor = db.query(
-//                ComfiContract.TransactionEntry.TABLE_NAME,   // The table to query
-//                projection,             // The array of columns to return (pass null to get all)
-//                selection,              // The columns for the WHERE clause
-//                selectionArgs,          // The values for the WHERE clause
-//                null,          // don't group the rows
-//                null,           // don't filter by row groups
-//                sortOrder              // The sort order
-//        );
-//
-//        List recentExpenses = new ArrayList<>();
-//        while (cursor.moveToNext()){
-//            Transaction transaction = new Transaction(cursor.getInt(0), cursor.getString(1))
-//        }
-//
-//
-//        return recentExpenses;
-//    }
+    public ArrayList<Transaction> getRecentExpenses(int dateInMiliSeconds){
+        if(cmDbHelper == null){
+            cmDbHelper = new ComfiDbHelper(context);
+        }
+
+        SQLiteDatabase db = cmDbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                ComfiContract.TransactionEntry.COLUMN_NAME_ID,
+                ComfiContract.TransactionEntry.COLUMN_NAME_TITLE,
+                ComfiContract.TransactionEntry.COLUMN_NAME_AMOUNT,
+                ComfiContract.TransactionEntry.COLUMN_NAME_DATE,
+        };
+
+        // Filter results WHERE "title" = 'My Title'
+        String selection = ComfiContract.TransactionEntry.COLUMN_NAME_TYPE + " = ?" + " AND " + ComfiContract.TransactionEntry.COLUMN_NAME_DATE + " <= " + dateInMiliSeconds;
+        String[] selectionArgs = {ComfiContract.TransactionEntry.DEFAULT_EXPENSE_STRING};
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                ComfiContract.TransactionEntry.COLUMN_NAME_DATE + " ASC";
+
+        Cursor cursor = db.query(
+                ComfiContract.TransactionEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,          // don't group the rows
+                null,           // don't filter by row groups
+                sortOrder,              // The sort order
+                "5"                   //limit
+        );
+
+        ArrayList<Transaction> recentExpenses = new ArrayList<>();
+        while (cursor.moveToNext()){
+            Transaction transaction = new Transaction(
+                    cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getString(3),
+                    cursor.getInt(4));
+            recentExpenses.add(transaction);
+        }
+        cursor.close();
+
+        return recentExpenses;
+    }
 }
