505331896d4c82a7461d4a206e2a1caa3c8c837b
jones270 <mitchelljones270@gmail.com>
2018-03-18 14:24:36 -0300

    ComfiDBhelper(update)

--- app/src/main/java/sr/unasat/financeapp/fragments/TransactionFragment.java
+++ app/src/main/java/sr/unasat/financeapp/fragments/TransactionFragment.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import sr.unasat.financeapp.R;
+import sr.unasat.financeapp.activities.MainActivity;
+import sr.unasat.financeapp.dao.TransactionDao;
 
 /**
  * A simple {@link Fragment} subclass.
