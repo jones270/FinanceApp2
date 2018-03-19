3d1f03891747162c78cda5886a2d7588f3815d84
jones270 <mitchelljones270@gmail.com>
2018-03-19 12:37:55 -0300

    Welcome screens

---
+++ app/src/main/java/sr/unasat/financeapp/helpers/PrefManager.java
@@ -0,0 +1,39 @@
+package sr.unasat.financeapp.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Mitchell Jones on 3/19/2018.
+ */
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
+
